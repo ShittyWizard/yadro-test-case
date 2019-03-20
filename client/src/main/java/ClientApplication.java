61a4bcc0fee4661453686e1bc05faefed46f0aa7
ShittyWizard <anisimov.develop@gmail.com>
2019-03-20 20:50:42 +0300

    Edit client application

--- client/src/main/java/ClientApplication.java
+++ client/src/main/java/ClientApplication.java
@@ -1,8 +1,23 @@
 import commands.CliNet;
 import picocli.CommandLine;
 
+import java.util.Scanner;
+
 public class ClientApplication {
     public static void main(String[] args) {
-        CommandLine.run(new CliNet(), args);
+        Scanner sc = new Scanner(System.in);
+        String[] arguments;
+        System.out.println("Welcome to cli_net!");
+        System.out.println("You can exit from cli_net by command 'exit' ");
+        System.out.println("Enter your command:");
+        while (sc.hasNext()) {
+            String line = sc.nextLine();
+            if (!line.equals("exit")) {
+                arguments = line.split(" ");
+                CommandLine.run(new CliNet(), arguments);
+            } else {
+                break;
+            }
+        }
     }
 }
